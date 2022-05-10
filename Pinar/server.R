b03b2f261b7ca4a202c5226bf65dcd26e8188c26
Pinar2022 <101690316+Pinar2022@users.noreply.github.com>
2022-05-10 22:41:11 +0900

    files

---
+++ Pinar/server.R
@@ -0,0 +1,53 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(kableExtra)
+library(xtable)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+    evs_17_clean <- readRDS("evs_17_clean.rds")
+    data_full<-evs_17_clean
+    
+
+    output$plot1 <- renderPlot({
+    
+            data_full %>%
+              filter(cntry == input$Countries &
+                  sex == input$Sex & 
+                        edu == input$Education) %>%
+            ggplot(aes_string("age", input$YVariable )) +
+            stat_summary(geom = "line", fun = "mean") +
+            theme_minimal() +
+            labs(x = "Age",
+                 y = "Average agreement",
+                 caption = paste("EVS 2017 from", input$Countries,
+                                 " - sex = ", input$Sex,
+                                 " - Education = ", input$Education))
+        
+    })
+    
+
+    output$regression <- renderPrint({
+        data_clean2 <- data_full %>%
+            filter(cntry == input$Countries)
+            
+        myformula  <- as.formula(paste0( input$YVariable, " ~ age + sex + edu"))
+        m1 <- lm( myformula, data = data_clean2)
+        summary(m1)
+        # xtable(summary(m1)) %>%
+        #     xtable(summary(m1)) %>%
+        #     kable(digits=3) %>%
+        #     kable_styling()
+    })
+
+})
---
+++ Pinar/ui.R
@@ -0,0 +1,53 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+list.countries <- c("Albania", "Azerbaijan", "Austria", "Armenia" ,"Bosnia and Herzegovina", "Bulgaria", "Belarus", "Croatia", "Czechia", "Denmark", "Estonia", "Finland", "France", "Georgia", "Germany", "Hungary", "Iceland", "Italy", "Lithuania", "Montenegro", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Russia", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "North Macedonia", "Great Britain" )
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Ochange that"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+           
+           tags$h3("Overview"),
+            p("Select a country from the drop down menu, and a survey topic to explore to visualize the citizens
+              attitudes towards the chosen topic."),
+            selectInput(inputId = 'Countries',
+                        label = "Select country:",
+                        choices = list.countries),
+           
+           selectInput(inputId = 'YVariable',
+                       label = "Select Y:",
+                       choices = c("child_suffer", "job_national")),
+            
+            radioButtons(inputId = "Sex" ,
+                         label = "Select sex",
+                         choices =  c("Female", "Male")), 
+           
+           radioButtons(inputId = "Education" ,
+                        label = "Select education",
+                        choices =  c("Lower", "Medium" , "Higher"))
+           
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Plot", plotOutput("plot1")),
+                tabPanel("Regression", verbatimTextOutput("regression"))
+            )
+        )
+    )
+))
